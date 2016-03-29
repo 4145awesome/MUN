930598ae1a083cbc0838bff36a242b9b7f2de8a9
Kyle Ramey <codefusion1@gmail.com>
2016-03-29 16:41:59 -0300

    Added json responses

--- app/Http/Controllers/MUNcontroller.php
+++ app/Http/Controllers/MUNcontroller.php
@@ -2,16 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Response;
+use Illuminate\Http\Request;
+
 class MunController extends Controller
 {
 
     private $insuranceUrl = 'https://insinc.laboratory.cf';
 
     public function getProperty($MlsID, Request $request){
-        $code = app('db')->table('munCodes')->where('mlsid', $MlsID)->first();
+        $code = app('db')->table('codes')->select('muncode')->where('mlsid', $MlsID)->first();
         $mortID = $request->input('mortID');
         if($code){
-            Request::create($this->insuranceUrl, 'POST', ['MortID' => $mortID, 'munCode' => $code]);
+            if($request->input('debug') == 1){
+                return response()->json(["error" => false, "response" =>["mortID" => $mortID, 'munCode' => $code->muncode]]);
+            }else {
+                Request::create($this->insuranceUrl, 'POST', ['mortID' => $mortID, 'munCode' => $code->muncode]);
+                return response()->json(["error" => false]);
+            }
+        }else{
+            return response()->json(["error" => "Could not find code"], 500);
         }
     }
 }
