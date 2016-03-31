00f99e54da46571d3f05085399edd3d6b84f13fc
Kyle Ramey <codefusion1@gmail.com>
2016-03-31 16:37:09 -0300

    should be working

--- app/Http/Controllers/MUNcontroller.php
+++ app/Http/Controllers/MUNcontroller.php
@@ -4,21 +4,31 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Response;
 use Illuminate\Http\Request;
+use GuzzleHttp\Client;
 
 class MunController extends Controller
 {
 
-    private $insuranceUrl = 'https://insinc.laboratory.cf';
+    private $insuranceUrl = 'http://localhost:8080/muncode';
 
     public function getProperty($MlsID, Request $request){
         $code = app('db')->table('codes')->select('muncode')->where('mlsid', $MlsID)->first();
         $mortID = $request->input('mortID');
         if($code){
             if($request->input('debug') == 1){
-                return response()->json(["error" => false, "response" =>["mortID" => $mortID, 'munCode' => $code->muncode]]);
+                return response()->json(["error" => false, "response" =>["mlsid" => $mortID, 'munCode' => $code->muncode]]);
             }else {
-                Request::create($this->insuranceUrl, 'POST', ['mortID' => $mortID, 'munCode' => $code->muncode]);
-                return response()->json(["error" => false]);
+                $client = new Client();
+                $response = $client->request('POST', $this->insuranceUrl, ['form_params' => ['mortId' => 12443, 'munCode' => $code->muncode]]);
+                $error = false;
+                $body = "No Data";
+                if($response->getStatusCode() != 200){
+                    $error = true;
+                    $body = $response->getReasonPhrase();
+                }else{
+                    $body = $response->getBody();
+                }
+                return response()->json(["error" => false, "response" => json_decode((string) $body)]);
             }
         }else{
             return response()->json(["error" => "Could not find code"], 500);
